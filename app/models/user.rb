26a0afbdca0ac6bd1438be53be055a99038b3f56
Ivan Munguia <xxthecreatorxx_live@hotmail.com>
2015-07-26 06:47:45 +0000

    Add test for lower-case email and a better regex (solves exercises 6.1, 6.2, 6.3)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ActiveRecord::Base
   
   has_secure_password
   
-  before_save { self.email = email.downcase }
+  before_save { self.email.downcase! }
   
   validates :name,  presence: true,
                     length: { maximum: 50 }
   
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true,
                     length: { maximum: 50 },
                     format: { with: VALID_EMAIL_REGEX },
