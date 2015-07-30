d1bbe408841acec82f3f4e1974b8755add675e6e
Ivan Munguia <xxthecreatorxx_live@hotmail.com>
2015-07-30 05:26:00 +0000

    Finish user edit, update, index and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,8 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 },
+                       allow_blank: true
   
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
