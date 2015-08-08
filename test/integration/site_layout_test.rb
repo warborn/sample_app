135896bd49b42f1a05bad37bbe13c2e9c2d28def
Ivan Munguia <xxthecreatorxx_live@hotmail.com>
2015-08-08 23:11:17 +0000

    Add test to layouts for logged and non logged in users (solves exercise 9.2)

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,12 +1,22 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
- test "layout links" do
-   get root_path
-   assert_template 'static_pages/home'
-   assert_select "a[href=?]", root_path, count: 2
-   assert_select "a[href=?]", help_path
-   assert_select "a[href=?]", about_path
-   assert_select "a[href=?]", contact_path
- end
+ 
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", login_path
+    user = users(:michael)
+    log_in_as(user)
+    get root_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(user)
+    assert_select "a[href=?]", edit_user_path(user)
+    assert_select "a[href=?]", logout_path
+  end
+
 end
